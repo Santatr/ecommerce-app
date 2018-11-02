e194a07b39dd5aa9690c5131b8d19518c21f0c22
Loredana Spataru <loredana.spataru@softvision.ro>
2018-11-02 14:00:13 +0200

    Add redux and create the store

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+document.getElementById('root'));
